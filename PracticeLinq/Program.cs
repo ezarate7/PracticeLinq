e0e5d798697774e13fe9834749547a32dba3e87b
Eddie Z <e.zarate7@hotmail.com>
2022-08-26 15:04:10 -0600

    initial commit

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+List<string> videoGames = new List<string>() { "apex Legends", "COD", "Fifa", "PubG"};
+
+videoGames.OrderBy(x => x.Length).ToList().ForEach(game => Console.WriteLine(game));
+Console.WriteLine("------");
+
+videoGames.Where(x => x.Length > 6).ToList().ForEach(game => Console.WriteLine(game));
+Console.WriteLine("----");
+videoGames.OrderBy(x => x.Length).ToList().ForEach(game => Console.WriteLine(game));
+Console.WriteLine("------");
+videoGames.OrderByDescending(x => x.Length).ToList().ForEach(game => Console.WriteLine(game));
+Console.WriteLine("-------");
+videoGames.Count(x => x.Length == 4).ToString().ToList().ForEach(x => Console.WriteLine(x));
+Console.WriteLine("----");
+
+
